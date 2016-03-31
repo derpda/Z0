b63295b47929e8f0ef167159fda10827c60eef80
Peter <peter.spalthoff@gmail.com>
2016-03-31 17:05:38 +0200

    new efficiency matrix added to useMatrix, unneeded lines removed

--- code/useMatrix.C
+++ code/useMatrix.C
@@ -7,30 +7,28 @@ using namespace std;
 
 void useMatrix(){
   //Filling a matrix
-	
-	const int n_invmass=7;
-	float factors[n_invmass]={0.308238,0.45956,0.589088,0.701125,0.684967,0.664385,0.583014};
 	ofstream matrixprint;
 	matrixprint .open("../results/matrix/matrices.txt");
 	
 	
 	TMatrixD matrix(4,4);
-matrix(0,0)=0.390898;
-matrix(0,1)=0.000181233;
-matrix(0,2)=0.00300633;
-matrix(0,3)=7.46253e-005;
-matrix(1,0)=2.11907e-005;
-matrix(1,1)=0.901707;
-matrix(1,2)=0.0242104;
-matrix(1,3)=1.05954e-005;
-matrix(2,0)=0.00441841;
-matrix(2,1)=0.00611003;
-matrix(2,2)=0.919623;
-matrix(2,3)=0.00686747;
-matrix(3,0)=2.02916e-005;
-matrix(3,1)=1.01458e-005;
-matrix(3,2)=0.00153202;
-matrix(3,3)=0.989702;
+	matrix(0, 0) = 0.390898;
+	matrix(0, 1) = 0.000181233;
+	matrix(0, 2) = 0.00251594;
+	matrix(0, 3) = 7.46253e-005;
+	matrix(1, 0) = 2.11907e-005;
+	matrix(1, 1) = 0.901707;
+	matrix(1, 2) = 0.0215086;
+	matrix(1, 3) = 1.05954e-005;
+	matrix(2, 0) = 0.00441841;
+	matrix(2, 1) = 0.00611003;
+	matrix(2, 2) = 0.488664;
+	matrix(2, 3) = 0.00686747;
+	matrix(3, 0) = 2.02916e-005;
+	matrix(3, 1) = 1.01458e-005;
+	matrix(3, 2) = 1.01458e-005;
+	matrix(3, 3) = 0.989702;
+
 	
 	matrixprint <<"Original matrix\n" << endl;
 	//print original matrix
