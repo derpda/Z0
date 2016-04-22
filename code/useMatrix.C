03a179e3a960d876063086407659fb33fcf34d59
Benjamin Winkelmann <Ben_Win@web.de>
2016-04-22 14:06:51 +0200

    neuausgeführt

--- code/useMatrix.C
+++ code/useMatrix.C
@@ -10,24 +10,23 @@ void useMatrix(){
 	ofstream matrixprint;
 	matrixprint .open("../results/matrix/matrices.txt");
 	
-	
 	TMatrixD matrix(4,4);
-	matrix(0, 0) = 0.390898;
+	matrix(0, 0) = 0.390781;
 	matrix(0, 1) = 2.11907e-005;
-	matrix(0, 2) = 0.00441841;
+	matrix(0, 2) = 0.00421643;
 	matrix(0, 3) = 2.02916e-005;
 	matrix(1, 0) = 0.000181233;
 	matrix(1, 1) = 0.901707;
 	matrix(1, 2) = 0.00611003;
 	matrix(1, 3) = 1.01458e-005;
 	matrix(2, 0) = 0.000394448;
-	matrix(2, 1) = 0.00945105;
-	matrix(2, 2) = 0.838766;
-	matrix(2, 3) = 0.000963851;
+	matrix(2, 1) = 0.00939808;
+	matrix(2, 2) = 0.834133;
+	matrix(2, 3) = 0.000943559;
 	matrix(3, 0) = 7.46253e-005;
 	matrix(3, 1) = 1.05954e-005;
-	matrix(3, 2) = 0.00686747;
-	matrix(3, 3) = 0.989702;
+	matrix(3, 2) = 0.00685485;
+	matrix(3, 3) = 0.984365;
 
 
 	
